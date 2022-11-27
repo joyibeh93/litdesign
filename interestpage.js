c226b1f1e11257982708e59c8a1d80c0666b2496
JOYCE <joychinwenduibeh93@gmail.com>
2022-11-27 09:40:36 +0100

    added javascrip and styles

---
+++ interestpage.js
@@ -0,0 +1,166 @@
+// let linkClick=document.querySelectorAll("p");
+// linkClick.addEventListener("click", function onclick(){
+//     linkClick.style.backgroundColor="#502F8A";
+//     linkClick.style.color="white";
+// });
+// alert("joy");
+
+let paragraph1 = document.querySelector("#interest-btn1");
+paragraph1.addEventListener("click", color => {
+    paragraph1.style.backgroundColor = "#502F8A";
+    paragraph1.style.color = "#fff"
+})
+
+let paragraph2 = document.querySelector("#interest-btn2");
+paragraph2.addEventListener("click", color => {
+    paragraph2.style.backgroundColor = "#502F8A";
+    paragraph2.style.color = "#fff"
+})
+let paragraph3 = document.querySelector("#interest-btn3");
+paragraph3.addEventListener("click", color => {
+    paragraph3.style.backgroundColor = "#502F8A";
+    paragraph3.style.color = "#fff"
+})
+
+let paragraph4 = document.querySelector("#interest-btn4");
+paragraph4.addEventListener("click", color => {
+    paragraph4.style.backgroundColor = "#502F8A";
+    paragraph4.style.color = "#fff"
+});
+
+let paragraph5 = document.querySelector("#interest-btn5");
+paragraph5.addEventListener("click", color => {
+    paragraph5.style.backgroundColor = "#502F8A";
+    paragraph5.style.color = "#fff"
+});
+
+let paragraph6 = document.querySelector("#interest-btn6");
+paragraph6.addEventListener("click", color => {
+    paragraph6.style.backgroundColor = "#502F8A";
+    paragraph6.style.color = "#fff"
+});
+let paragraph7 = document.querySelector("#interest-btn7");
+paragraph7.addEventListener("click", color => {
+    paragraph7.style.backgroundColor = "#502F8A";
+    paragraph7.style.color = "#fff"
+});
+let paragraph8 = document.querySelector("#interest-btn8");
+paragraph8.addEventListener("click", color => {
+    paragraph8.style.backgroundColor = "#502F8A";
+    paragraph8.style.color = "#fff"
+});
+let paragraph9 = document.querySelector("#interest-btn9");
+paragraph9.addEventListener("click", color => {
+    paragraph9.style.backgroundColor = "#502F8A";
+    paragraph9.style.color = "#fff"
+})
+let paragraph10 = document.querySelector("#interest-btn10");
+paragraph10.addEventListener("click", color => {
+    paragraph10.style.backgroundColor = "#502F8A";
+    paragraph10.style.color = "#fff"
+})
+let paragraph11 = document.querySelector("#interest-btn11");
+paragraph11.addEventListener("click", color => {
+    paragraph11.style.backgroundColor = "#502F8A";
+    paragraph11.style.color = "#fff"
+})
+let paragraph12 = document.querySelector("#interest-btn12");
+paragraph12.addEventListener("click", color => {
+    paragraph12.style.backgroundColor = "#502F8A";
+    paragraph12.style.color = "#fff"
+})
+let paragraph13 = document.querySelector("#interest-btn13");
+paragraph13.addEventListener("click", color => {
+    paragraph13.style.backgroundColor = "#502F8A";
+    paragraph13.style.color = "#fff"
+})
+let paragraph14 = document.querySelector("#interest-btn14");
+paragraph14.addEventListener("click", color => {
+    paragraph14.style.backgroundColor = "#502F8A";
+    paragraph14.style.color = "#fff"
+})
+let paragraph15 = document.querySelector("#interest-btn15");
+paragraph15.addEventListener("click", color => {
+    paragraph15.style.backgroundColor = "#502F8A";
+    paragraph15.style.color = "#fff"
+})
+let paragraph16 = document.querySelector("#interest-btn16");
+paragraph16.addEventListener("click", color => {
+    paragraph16.style.backgroundColor = "#502F8A";
+    paragraph16.style.color = "#fff"
+})
+let paragraph17 = document.querySelector("#interest-btn17");
+paragraph17.addEventListener("click", color => {
+    paragraph17.style.backgroundColor = "#502F8A";
+    paragraph17.style.color = "#fff"
+})
+let paragraph18 = document.querySelector("#interest-btn18");
+paragraph18.addEventListener("click", color => {
+    paragraph18.style.backgroundColor = "#502F8A";
+    paragraph18.style.color = "#fff"
+})
+let paragraph19 = document.querySelector("#interest-btn19");
+paragraph19.addEventListener("click", color => {
+    paragraph19.style.backgroundColor = "#502F8A";
+    paragraph19.style.color = "#fff"
+})
+let paragraph20 = document.querySelector("#interest-btn20");
+paragraph20.addEventListener("click", color => {
+    paragraph20.style.backgroundColor = "#502F8A";
+    paragraph20.style.color = "#fff"
+})
+let paragraph21 = document.querySelector("#interest-btn21");
+paragraph21.addEventListener("click", color => {
+    paragraph21.style.backgroundColor = "#502F8A";
+    paragraph21.style.color = "#fff"
+})
+let paragraph22 = document.querySelector("#interest-btn22");
+paragraph22.addEventListener("click", color => {
+    paragraph22.style.backgroundColor = "#502F8A";
+    paragraph22.style.color = "#fff"
+})
+let paragraph23 = document.querySelector("#interest-btn23");
+paragraph23.addEventListener("click", color => {
+    paragraph23.style.backgroundColor = "#502F8A";
+    paragraph23.style.color = "#fff"
+})
+let paragraph24 = document.querySelector("#interest-btn24");
+paragraph24.addEventListener("click", color => {
+    paragraph24.style.backgroundColor = "#502F8A";
+    paragraph24.style.color = "#fff"
+})
+let paragraph25 = document.querySelector("#interest-btn25");
+paragraph25.addEventListener("click", color => {
+    paragraph25.style.backgroundColor = "#502F8A";
+    paragraph25.style.color = "#fff"
+})
+let paragraph26 = document.querySelector("#interest-btn26");
+paragraph26.addEventListener("click", color => {
+    paragraph26.style.backgroundColor = "#502F8A";
+    paragraph26.style.color = "#fff"
+})
+let paragraph27 = document.querySelector("#interest-btn27");
+paragraph27.addEventListener("click", color => {
+    paragraph27.style.backgroundColor = "#502F8A";
+    paragraph27.style.color = "#fff"
+})
+let paragraph28 = document.querySelector("#interest-btn28");
+paragraph28.addEventListener("click", color => {
+    paragraph28.style.backgroundColor = "#502F8A";
+    paragraph28.style.color = "#fff"
+})
+let paragraph29 = document.querySelector("#interest-btn29");
+paragraph29.addEventListener("click", color => {
+    paragraph29.style.backgroundColor = "#502F8A";
+    paragraph29.style.color = "#fff"
+})
+let paragraph30 = document.querySelector("#interest-btn30");
+paragraph30.addEventListener("click", color => {
+    paragraph30.style.backgroundColor = "#502F8A";
+    paragraph30.style.color = "#fff"
+})
+let paragraph31 = document.querySelector("#interest-btn31");
+paragraph31.addEventListener("click", color => {
+    paragraph31.style.backgroundColor = "#502F8A";
+    paragraph31.style.color = "#fff"
+})
